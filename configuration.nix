73da3a377b6b8aeba518266bfd6e196293502090
ValMzn <VManzon@horizontalsoftware.com>
2024-07-28 19:31:35 +0200

    fix

--- configuration.nix
+++ configuration.nix
@@ -15,8 +15,7 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gnome.enable = true;
-
+  services.xserver.desktopManager.gnome.enable = true;
 
   users.users.valmzn = {
     isNormalUser = true;
--- install.sh
+++
@@ -1,8 +0,0 @@
-nix --experimental-features "nix-command flakes" \
-  run github:nix-community/disko -- \
-  --mode disko /tmp/disko.nix \
-  --arg device '"/dev/sda"'
-
-nixos-generate-config --root /mnt
-mv ./configuration.nix /mnt/etc/nixos/configuration.nix
-nixos-install --root /mnt
